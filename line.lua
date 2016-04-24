f6036e586cad6748ced390af8f052390b4ba7b60
Abe Hiroshi <abhrsh@gmail.com>
2016-04-24 17:47:08 +0000

    send message

--- line.lua
+++ line.lua
@@ -1,7 +1,17 @@
 local m = {}
 
-function m.message()
-    return "hoge2"
+function m.message(channel, data)
+    return http.request {
+	    url = 'https://trialbot-api.line.me/v1/events',
+	    method = 'POST',
+	    headers = {
+	    	['Content-Type'] = 'application/json; charset=UTF-8',
+	       	['X-Line-ChannelID'] = channel.id,
+	    	['X-Line-ChannelSecret'] = channel.secret,
+	    	['X-Line-Trusted-User-With-ACL'] = channel.mid,
+	    },
+	    data = json.stringify(data),
+    }
 end
 
 return m
