02c0344f8da72ec856a85a3bec43e14dc7151627
Abe Hiroshi <abhrsh@gmail.com>
2016-04-26 13:09:56 +0000

    add sortedPairs function

--- util.lua
+++ util.lua
@@ -12,4 +12,14 @@ function m.urlencode(str)
     return str
 end
 
+-- テーブルをキーでソートされた{key,value}の配列に変換
+function m.sortedPairs(t)
+    local keyvals = {}
+    for k, v in pairs(t) do
+    	table.insert(keyvals, {key=k, val=v})
+    end
+    table.sort(keys, function(a, b) return a.key < b.key end)
+    return ipairs(keys)
+end
+
 return m
