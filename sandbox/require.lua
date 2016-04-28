feb45e37fa77b8452f85bec5fa0f2dacd6e05181
Abe Hiroshi <abhrsh@gmail.com>
2016-04-28 16:17:05 +0000

    fix module

---
+++ sandbox/require.lua
@@ -0,0 +1,16 @@
+local line = require 'abehiroshi/webscriptio-lib/line'
+
+local m = {}
+
+function m.message(){
+    line.message(json.parse(storage.linebot_keys or {}), {
+    		to = {storage.linebot_my_mid},
+    		content = {
+    			contentType = 1,
+    			toType = 1,
+    			text = 'できた？',
+    		},
+    })
+}
+
+return m
