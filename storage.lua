487c7cdefde81276dee32c1a22e65bf71f39a53b
Abe Hiroshi <abhrsh@gmail.com>
2016-05-10 16:11:00 +0000

    function

--- storage.lua
+++ storage.lua
@@ -16,7 +16,75 @@ function queue.push(self, x)
 	lease.release(self.count)
 end
 
+-- キューから取り出し
+function queue.pop(self)
+	lease.acquire(self.head)
 
+	local header = storage[self.head] or 1
+	local ret = storage[self.store..header]
+	if ret then
+		storage[self.head] = header + 1
+	elseif storage[self.head] then
+		-- 何もなければstorageをクリア
+		lease.acquire(self.count)
+			if (storage[self.count] or 0) - header < 0 then
+				storage[self.count] = nil
+				storage[self.head] = nil
+			end
+		lease.release(self.count)
+	end
+
+	lease.release(self.head)
+	
+	if ret then
+		storage[self.store..header] = nil
+		return json.parse(ret)
+	else
+		return nil
+	end
+end
+
+-- キューの件数
+function queue.count(self)
+	return (storage[self.count] or 0) - (storage[self.head] or 1) + 1
+end
+
+-- キューをクリア
+function queue.clear(self)
+	lease.acquire(self.head)
+	lease.acquire(self.count)
+
+	local header = storage[self.head] or 1
+	local counter = (storage[self.count] or 0)
+	for i = header, counter do
+		storage[self.store..i] = nil
+	end
+	storage[self.count] = nil
+	storage[self.head] = nil
+
+	lease.release(self.count)
+	lease.release(self.head)
+end
+
+function queue.head(self, offset)
+	local header = (storage[self.head] or 1) + (offset or 0)
+	local ret = storage[self.store..header]
+	if ret then
+		return json.parse(ret)
+	else
+		return nil
+	end
+end
+
+function queue.last(self, offset)
+	local counter = (storage[self.count] or 0) - (offset or 0)
+	local ret = storage[self.store..counter]
+	if ret then
+		return json.parse(ret)
+	else
+		return nil
+	end
+end
 
 -- キューを作成
 function m.queue(id, prefix)
@@ -26,85 +94,25 @@ function m.queue(id, prefix)
 		head = (prefix or '')..'queue_head_'..id,
 		capacity = (prefix or '')..'queue_count_'..id,
 	}
-	local count = self.count
-	local store = self.store
-	local head = self.head
-	local capacity = self.capacity
-	
-	storage[capacity] = storage[capacity] or 0
+	storage[self.capacity] = storage[self.capacity] or 0
 	
 	return {
 		-- キューに追加
 		push = function(x) return queue.push(self, x) end,
-
 		-- キューから取り出し
-		pop = function()
-			lease.acquire(head)
-				local header = storage[head] or 1
-				local ret = storage[store..header]
-				if ret then
-					storage[head] = header + 1
-				elseif storage[head] then
-					-- 何もなければstorageをクリア
-					lease.acquire(count)
-						if (storage[count] or 0) - header < 0 then
-							storage[count] = nil
-							storage[head] = nil
-						end
-					lease.release(count)
-				end
-			lease.release(head)
-			
-			if ret then
-				storage[store..header] = nil
-				return json.parse(ret)
-			else
-				return nil
-			end
-		end,
-
-		-- キューの数
-		count = function()
-			return (storage[count] or 0) - (storage[head] or 1) + 1
-		end,
-
+		pop = function() return queue.pop(self) end,
+		-- キューの件数
+		count = function() return queue.count(self) end,
 		-- キューをクリア
-		clear = function()
-			lease.acquire(head)
-			lease.acquire(count)
-				local header = storage[head] or 1
-				local counter = (storage[count] or 0)
-				for i = header, counter do
-					storage[store..i] = nil
-				end
-				storage[count] = nil
-				storage[head] = nil
-			lease.release(count)
-			lease.release(head)
-		end,
-
-		head = function(index)
-			local header = (storage[head] or 1) + (index or 0)
-			local ret = storage[store..header]
-			if ret then
-				return json.parse(ret)
-			else
-				return nil
-			end
-		end,
+		clear = function() return queue.clear(self) end,
+		-- 先頭の要素を参照
+		head = function(offset) return queue.head(self, offset) end,
+		-- 末尾の要素を参照
+		last = function(offset) return queue.last(self, offset) end,
 		
-		last = function(index)
-			local counter = (storage[count] or 0) - (index or 0)
-			local ret = storage[store..counter]
-			if ret then
-				return json.parse(ret)
-			else
-				return nil
-			end
-		end,
-		
+		-- 容量を設定
 		setCapacity = function(n)
-			storage[capacity] = tonumber(n) or storage[capacity]
+			storage[self.capacity] = tonumber(n) or storage[self.capacity]
 		end,
 	}
 end
