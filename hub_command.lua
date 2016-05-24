f1fce92f201a2e3aea0029000e727d7fa07b5d30
Abe Hiroshi <abhrsh@gmail.com>
2016-05-24 16:03:19 +0000

    add command to hub

---
+++ hub_command.lua
@@ -0,0 +1,39 @@
+-- hubにコマンド登録
+
+local m = {}
+
+local hub = require 'hub'
+
+-- hubにコマンドを登録する
+function m.add(commands)
+    if type(commands) == 'string' then
+        commands = {commands}
+    end
+    for i,v in ipairs(commands) do
+        if command[v] then
+            hub.add_command(v, command[v])
+        end
+    end
+end
+
+-- コマンド
+local command = {}
+
+-- Amazon商品検索
+function command.amazon_itemsearch(self, args)
+	local amazon = require 'amazon'
+	local ret = amazon.itemsearch {
+		info = self.amazon_info,
+		params = {Keywords = args},
+	}
+	return ret, ret.error
+end
+
+-- Line送信
+function command.line(self, args)
+	local line = require 'line'
+	args.info = self.line_info
+	line.send(args)
+end
+
+return m
