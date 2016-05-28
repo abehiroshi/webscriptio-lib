3cd6c83c68d4f0e7c8b470b9a8097e65c24d1d8d
Abe Hiroshi <abhrsh@gmail.com>
2016-05-28 16:49:12 +0000

    add hook

--- storagify.lua
+++ storagify.lua
@@ -3,25 +3,37 @@
 local m = {}
 
 -- storageアクセス用のmetatableを作成する
-function metastorage(prefix, struct)
+function metastorage(prefix, struct, hook)
 	local p = prefix..'/'
 	for k,v in pairs(struct) do
-		setmetatable(v, metastorage(p..k, v))
+		setmetatable(v, metastorage(p..k, v, hook))
 	end
+	local decode = hook and hook.decode
+	local encode = hook and hook.encode
 	return {
 		__index = function(table, index)
-			return (struct and rawget(struct, index)) or storage[p..index]
+			local v = (struct and rawget(struct, index)) or storage[p..index]
+			if decode then
+			    return decode(v)
+			else
+			    return v
+			end
 		end,
+
 		__newindex = function(table, index, value)
-			storage[p..index] = value
+		    local v = value
+		    if encode then
+		        v = encode(v)
+	        end
+			storage[p..index] = v
 		end,
 	}
 end
 
 -- storageアクセス用オブジェクトを作成する
-function m.create(prefix, struct)
+function m.create(prefix, struct, hook)
 	local s = struct or {}
-	setmetatable(s, metastorage(prefix, s))
+	setmetatable(s, metastorage(prefix, s, hook))
 	return s
 end
 
