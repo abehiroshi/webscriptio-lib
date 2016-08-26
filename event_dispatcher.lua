a72bc3e1619a5d474340f5bcedfaafc5c5ddcf29
Abe Hiroshi <abhrsh@gmail.com>
2016-08-26 12:02:26 +0000

    change hub base

--- event_dispatcher.lua
+++ event_dispatcher.lua
@@ -1,47 +1,51 @@
 -- Event dispatcher
 
-local m = {}
+local logger = function() end
 
 local template = require 'abehiroshi/webscriptio-lib/template'
 local memory = require 'abehiroshi/webscriptio-lib/memory'
 local hub = require 'abehiroshi/webscriptio-lib/hub_command'
 
-function hub_default(self, event)
-	self.context[event.name] = event.result
-	local status = (event.status and string.format(':%s', event.status)) or ''
-	if status == ':' then status = '' end
-    self._logger('event: '..event.name..status)
+function hub_default(memory_name)
+	local mem = memory.create(memory_name)
 
-	local command = assistant.data[event.name..status]
-	if command then
-		local commands = template.apply(command, {self = self,	event = event})
-		if #commands > 0 then
-			self:push(unpack(commands))
-		else
-			self:push(commands)
+	return function(self, event)
+		self.context[event.name] = event.result
+		local status = (event.status and string.format(':%s', event.status)) or ''
+		if status == ':' then status = '' end
+		local key = event.name..status
+	    logger('event: '..key)
+
+		local command = mem.data[key]
+		if command then
+			local commands = template.apply(command, {self = self,	event = event})
+			if #commands > 0 then
+				self:push(unpack(commands))
+			else
+				self:push(commands)
+			end
+	    else
+	        logger('no command: '..key)
 		end
-    else
-        self._logger('no command: '..event.name..status)
 	end
 end
 
-function m:use_logger(logger)
-    self._logger = logger
+function m.use_logger(_logger)
+    logger = _logger
 end
 
-function m:use_luatache(lustache)
-    template = template.use(lustache)
+function m.use_luatache(...)
+    template = template.use(...)
 end
 
-function m.create(name, hub_require, self)
-    hub.require(hub_require)
-    local h = hub.new(name, data)
-    h:on_default(hub_default)
+function m.hub_require(...)
+	hub.require(...)
+end
 
-    self._logger = function() end
-    self._hub = h
-    self._memory = memory.create(name)
-	return setmetatable(self, {__index = m})
+function m.create(name, self)
+	local h = hub.new(name, self)
+    h:on_default(hub_default(name))
+	return h
 end
 
 return m
