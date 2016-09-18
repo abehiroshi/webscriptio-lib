26da3ca58d42e35a069d690e3d48ccdf0f50c235
Abe Hiroshi <abhrsh@gmail.com>
2016-09-18 16:59:58 +0000

    Change types instead of keys

--- memory.lua
+++ memory.lua
@@ -9,12 +9,10 @@ local stringify = require 'stringify'
 function hook(self)
 	return {
 		decode = function(index, value)
-			return stringify.decode(self.keys[index], value)
+			return stringify.decode(self._meta.types[index], value)
 		end,
 
 		encode = function(index, value)
-			self.keys[index] = type(value)
-
 			local types = json.parse(self._meta.types or '{}')
 			if value == nil then
 				types[index] = nil
@@ -28,41 +26,26 @@ function hook(self)
 	}
 end
 
--- キー情報のメタテーブルを作成する
-function metakeys(storage_keys)
-	return {
-		__newindex = function(table, index, value)
-			rawset(table, index, value)
-			storage[storage_keys] = json.stringify(table)
-		end,
-	}
-end
-
--- キー情報をstorageから読み込む
-function loadkeys(storage_keys)
-	local keys_string = storage[storage_keys]
-	local keys = (keys_string and json.parse(keys_string)) or {}
-	return setmetatable(keys, metakeys(storage_keys))
-end
-
 -- 記憶をクリアする
-function m.clear(self)
-	for k,v in pairs(self.keys) do
-		storage[k] = nil
+function m:clear()
+	for k,v in pairs(json.parse(self._meta.types)) do
+		self.data[k] = nil
 	end
-	storage[self.storage_keys] = nil
-	self.keys = setmetatable({}, metakeys(self.storage_keys))
-
 	self._meta.types = nil
 end
 
 -- 記憶をダンプする
-function m.dump(self)
+function m:dump()
 	local d = {
-		[self.storage_keys] = storage[self.storage_keys]
+		_meta = {
+			prefix = self._meta.prefix,
+			struct = self._meta.struct,
+			types = self._meta.types,
+		},
+		data = {},
 	}
-	for k,v in pairs(self.keys) do
-		d[k] = storage[k]
+	for k,v in pairs(json.parse(d._meta.types)) do
+		d[k] = self.data[k]
 	end
 	return d
 end
@@ -78,9 +61,6 @@ end
 function m.create(prefix, struct)
 	local self = setmetatable({}, {__index = m})
 	self.data = storagify.create(prefix..'/data', struct, hook(self))
-
-	self.storage_keys = prefix..'/keys'
-	self.keys = loadkeys(self.storage_keys)
 
 	self._meta = storagify.create(prefix..'/meta')
 	self._meta.prefix = prefix
