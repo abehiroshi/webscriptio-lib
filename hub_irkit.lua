6232656b0e3a2c1501256f66e9f570f82f200f07
Abe Hiroshi <abhrsh@gmail.com>
2016-09-27 11:58:18 +0000

    Add hub_irkit

---
+++ hub_irkit.lua
@@ -0,0 +1,29 @@
+local hub = require 'hub'
+local irkit = require 'irkit'
+
+-- IRKit受信
+hub.add_command('irkit_receive', function(self, args)
+	locat ir = irkit.create(self.irkit)
+	local response = ir:receive()
+	local status = ''
+	if response.statuscode ~= 200 then
+		status = 'error'
+	end
+	if self.context and args and args.name then
+		self.context[name] = response.message
+	end
+	return response.message, status
+end)
+
+-- IRKit送信
+hub.add_command('irkit_send', function(self, args)
+	locat ir = irkit.create(self.irkit)
+	local response = ir:send(args.message)
+	local status = ''
+	if response.statuscode ~= 200 then
+		status = 'error'
+	end
+	return response.content, status
+end)
+
+return hub
