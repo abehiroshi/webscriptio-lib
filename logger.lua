795cc60fb6cb429638e897cfa610961cab138d02
Abe Hiroshi <abhrsh@gmail.com>
2016-10-11 11:34:22 +0000

    Add logger

---
+++ logger.lua
@@ -0,0 +1,24 @@
+-- ログ出力
+
+local m = {}
+
+local stringify = require 'stringify'
+
+local logger = function() end
+
+function m.init(fn)
+    logger = fn
+end
+
+-- インスタンス作成
+function m.get(category)
+    return function(...)
+        local text = os.date("![%Y/%m/%d %H:%M:%S]")..(category or '')
+        for i,v in ipairs({...}) do
+            text = text..' '..stringify.encode(v)
+        end
+        logger(text)
+    end
+end
+
+return m
