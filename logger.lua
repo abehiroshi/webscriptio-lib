d785c59bbcbc3c59f9ff174dad95df7cf2f17a10
Abe Hiroshi <abhrsh@gmail.com>
2016-10-11 11:37:51 +0000

    Change category log format

--- logger.lua
+++ logger.lua
@@ -12,8 +12,9 @@ end
 
 -- インスタンス作成
 function m.get(category)
+    category = '['..(category or '')..']'
     return function(...)
-        local text = os.date("![%Y/%m/%d %H:%M:%S]")..(category or '')
+        local text = os.date("![%Y/%m/%d %H:%M:%S]")..category
         for i,v in ipairs({...}) do
             text = text..' '..stringify.encode(v)
         end
