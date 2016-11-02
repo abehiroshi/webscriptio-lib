84f1176eef9a8601abd11ca1e6fad335e82b874e
Abe Hiroshi <abhrsh@gmail.com>
2016-11-02 15:46:19 +0000

    Fix return dump

--- hub_memory_google.lua
+++ hub_memory_google.lua
@@ -4,8 +4,8 @@ local memory_google = require 'memory_google'
 -- Line送信
 hub.add_command('load_memory_google', function(self, args)
     local mg = memory_google.create(args.memoryname)
-    mg:load(args.sheetname)
-	return loaded.dump()
+    local loaded = mg:load(args.sheetname)
+	return loaded:dump()
 end)
 
 return hub
