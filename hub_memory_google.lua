89ca446c90aed46c859fea59ceaf23fd3c709cbf
Abe Hiroshi <abhrsh@gmail.com>
2016-11-02 16:00:22 +0000

    Fix google key

--- hub_memory_google.lua
+++ hub_memory_google.lua
@@ -1,11 +1,11 @@
 local hub = require 'hub'
 local memory_google = require 'memory_google'
 
--- Line送信
+-- memoryをgoogle spreadsheet から読み込む
 hub.add_command('load_memory_google', function(self, args)
-    local mg = memory_google.create(args.memoryname)
+    local mg = memory_google.create(args.memoryname, self.store.google)
     local loaded = mg:load(args.sheetname)
-	return loaded:dump()
+	return loaded:dump(), ''
 end)
 
 return hub
