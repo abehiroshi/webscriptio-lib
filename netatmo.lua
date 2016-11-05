05524fd66717f83a9d2f2792116210e4eedcc8eb
Abe Hiroshi <abhrsh@gmail.com>
2016-11-05 20:33:09 +0900

    Add netatmo

---
+++ netatmo.lua
@@ -0,0 +1,30 @@
+
+local logger = (require 'logger').get('netatmo')
+local http_client = require 'http_client'
+
+local m = {}
+
+funcion m:oauth_token(args)
+    logger.info('oauth_token')
+    logger.debug(args)
+    local response = http_client.request {
+        url = 'https://api.netatmo.com/oauth2/token',
+        method = 'POST',
+        data = {
+            grant_type = 'password',
+            client_id = args.client_id,
+            client_secret = args.client_secret,
+            username = args.username,
+            password = args.password,
+        },
+    }
+    logger.debug('oauth_token end', response)
+    retuen response
+end
+
+
+function m.create(self)
+    return setmetatable(self, {__index = m})
+end
+
+return m
