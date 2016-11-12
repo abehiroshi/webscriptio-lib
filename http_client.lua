d1e046e7e46635d95948a9004c9452c38c0089c2
Abe Hiroshi <abhrsh@gmail.com>
2016-11-12 13:31:04 +0000

    Change application/json to tostring

--- http_client.lua
+++ http_client.lua
@@ -1,6 +1,7 @@
 -- HTTP Client
 
 local logger = (require 'logger').get('http_client')
+local stringify = require 'stringify'
 
 local m = {}
 
@@ -8,6 +9,13 @@ function m.request(params)
     logger.info('request start', params.url)
     logger.trace('request start', params)
 
+    if params and params.headers then
+        local content_type = params.headers['Content-Type']
+        if type(content_type) == 'string' and content_type:find('^application/json') then
+            logger.trace('Content-Type:', content_type)
+            params.data = stringify.encode(params.data)
+        end
+    end
     local response = http.request(params)
 
     if response then
