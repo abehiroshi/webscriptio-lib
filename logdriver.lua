dc911dead3589c4897eff464232b9b00234cadcd
Abe Hiroshi <abhrsh@gmail.com>
2016-11-19 13:21:28 +0000

    Fix logger

--- logdriver.lua
+++ logdriver.lua
@@ -1,6 +1,7 @@
 -- ログ運用
 
-local logger = require 'logger'
+local _logger = require 'logger'
+local logger = _logger.get('logdriver')
 local history = require 'history'
 local memory = require 'memory'
 
@@ -17,14 +18,14 @@ function m.load(memory_name)
             capacity = config.history_capacity,
         }
     )
-    logger.init(function(text)
+    _logger.init(function(text)
         log(text)
         hist:push(text)
     end)
 
-    logger.level(config.root_level)
+    _logger.level(config.root_level)
     for k,v in pairs(config.levels or {}) do
-        logger.level(v, k)
+        _logger.level(v, k)
     end
 
     return m
@@ -35,7 +36,7 @@ function m.view(args)
     logger.info('view', args)
     args = args or {}
 
-    logger.level(args.log_level or 'TRACE')
+    _logger.level(args.log_level or 'TRACE')
     local loghistory = history.create(args.history_name or 'log')
     local options = {
         max_count = args.max_count or 100,
